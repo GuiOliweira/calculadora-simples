4e991151d4f0695324c8e8a00bc199d81d4c0785
Guilherme Oliveira <xguilhermebolino51@gmail.com>
2024-07-09 10:40:53 -0300

    Funcao para dividir pronta, com a formatacao de apenas duas casas decimais para a projecao.

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def Menu():
             Multiplicar()
             break
         elif escolha == 4:
-            # Dividir()
+            Dividir()
             break
         elif escolha == 5:
             break
@@ -69,6 +69,25 @@ def Multiplicar():
         escolha = int(input("Deseja um novo cálculo? (1-Sim 2-Não): "))
         if escolha == 1:
             Multiplicar()
+            break
+        elif escolha == 2:
+            Menu()
+            break
+        else:
+            print("\nOpção inválida, tente novamente...\n")
+
+
+def Dividir():
+    n1 = int(input("Digite o primeiro numero: "))
+    n2 = int(input("Digite o segundo numero: "))
+    div = n1 / n2
+
+    print(f"\nA divisão de {n1} ÷ {n2} é: {div:.2f}\n")
+
+    while True:
+        escolha = int(input("Deseja um novo cálculo? (1-Sim 2-Não): "))
+        if escolha == 1:
+            Dividir()
             break
         elif escolha == 2:
             Menu()
