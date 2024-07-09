6b24046fff5ce3500c0e056f0daaa85cf6aa1420
Guilherme Oliveira <xguilhermebolino51@gmail.com>
2024-07-09 10:18:37 -0300

    Feita a primeira funcao de Menu para cada tipo de operacao.

---
+++ main.py
@@ -0,0 +1,42 @@
+def Menu():
+    while True:
+        print("\nMENU\n1- Somar\n2- Subtrair\n3- Multiplicar\n4- Dividir\n5- Sair\n")
+        escolha = int(input("Escolha uma das opções: "))
+        if escolha == 1:
+            Soma()
+            break
+        elif escolha == 2:
+            Subtrair()
+            break
+        elif escolha == 3:
+            Multiplicar()
+            break
+        elif escolha == 4:
+            Dividir()
+            break
+        elif escolha == 5:
+            break
+        else:
+            print("\nOpção inválida, tente novamente...\n")
+
+
+def Soma():
+    n1 = int(input("Digite o primeiro numero: "))
+    n2 = int(input("Digite o segundo numero: "))
+    soma = n1 + n2
+
+    print(f"\nA soma de {n1} + {n2} é: {soma}\n")
+
+    while True:
+        escolha = int(input("Deseja uma nova soma? (1-Sim 2-Não): "))
+        if escolha == 1:
+            Soma()
+            break
+        elif escolha == 2:
+            Menu()
+            break
+        else:
+            print("\nOpção inválida, tente novamente...\n")
+
+
+Menu()
